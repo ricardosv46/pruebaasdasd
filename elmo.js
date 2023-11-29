9deca21fbcca882df3fa8fe59821559b0a113aee
ricardo <ricardosv46@gmail.com>
2023-11-28 20:03:20 -0500

    fix

--- elmo.js
+++ elmo.js
@@ -1,19 +1,7 @@
 import React from 'react'
 
-const elmo = () => {
+export const elmo = () => {
   return (
     <div>elmo</div>
   )
 }
-
-export default elmo
-}
-import React from 'react'
-
-const elmo = () => {
-  return (
-    <div>elmo</div>
-  )
-}
-
-export default elmo
